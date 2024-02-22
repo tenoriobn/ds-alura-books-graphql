e07ca2f24cfa86b186a36f719114bf18c115b3ad
Bruno Tenório <tenoriobn@gmail.com>
2024-02-22 15:45:36 -0300

    tratando quantidade zerada

--- src/paginas/Carrinho/ItemCarrinho/index.tsx
+++ src/paginas/Carrinho/ItemCarrinho/index.tsx
@@ -12,9 +12,17 @@ interface ItemCarrinhoProps {
 }
 
 const ItemCarrinho = ({ item } : ItemCarrinhoProps) => {
-    const { adicionarItemCarrinho } = useCarrinhoContext();
+    const { adicionarItemCarrinho, removerItemCarrinho } = useCarrinhoContext();
 
     const alterarQuantidadeDoItem = (quantidade: number) => {
+        if (quantidade === 0) {
+            removerItemCarrinho({
+                livro: item.livro,
+                opcaoCompra: item.opcaoCompra,
+                quantidade
+            })
+        }
+
         adicionarItemCarrinho({
             livro: item.livro,
             opcaoCompra: item.opcaoCompra,
